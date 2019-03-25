8281a1497d227f8d1a451d778187e896058f9141
Super Edriver <superedriver@gmail.com>
2019-03-25 21:53:51 +0200

    added quotes section

--- public/js/slider.js
+++ public/js/slider.js
@@ -1,30 +1,36 @@
 window.onload = function () {
+    var headerSlider = document.querySelectorAll('.header-slider')[0];
+    startSlider(headerSlider);
+    var quotesSlider = document.querySelectorAll('.quotes-slider')[0];
+    startSlider(quotesSlider);
+};
+function startSlider(slider) {
     var interval = 3000;
     var slidesSelector = '.slide-list .slide-item';
     var slidesControlsSelector = '.toggle-list .toggle-item';
-    var slides = document.querySelectorAll(slidesSelector);
-    var slidesControls = document.querySelectorAll(slidesControlsSelector);
+    var slides = slider.querySelectorAll(slidesSelector);
+    var sliderControls = slider.querySelectorAll(slidesControlsSelector);
     var currentIndex = 0;
     var slideInterval = setInterval(nextSlide, interval);
-    slidesControls.forEach(function (item, index) {
+    sliderControls.forEach(function (item, index) {
         item.addEventListener('click', setCurrentSlide.bind(null, index));
     });
     function nextSlide() {
         slides[currentIndex].classList.remove('current');
-        slidesControls[currentIndex].classList.remove('current');
+        sliderControls[currentIndex].classList.remove('current');
         currentIndex = (currentIndex + 1) % slides.length;
         slides[currentIndex].classList.add('current');
-        slidesControls[currentIndex].classList.add('current');
+        sliderControls[currentIndex].classList.add('current');
     }
     function setCurrentSlide(index) {
         clearInterval(slideInterval);
-        document.querySelectorAll(slidesSelector + ".current")[0]
+        slider.querySelectorAll(slidesSelector + ".current")[0]
             .classList.remove('current');
-        document.querySelectorAll(slidesControlsSelector + ".current")[0]
+        slider.querySelectorAll(slidesControlsSelector + ".current")[0]
             .classList.remove('current');
         currentIndex = index;
         slides[currentIndex].classList.add('current');
-        slidesControls[currentIndex].classList.add('current');
+        sliderControls[currentIndex].classList.add('current');
         slideInterval = setInterval(nextSlide, interval);
     }
-};
+}
--- src/js/slider.ts
+++ src/js/slider.ts
@@ -1,38 +1,46 @@
 window.onload = function() {
-  const interval = 3000;
-  const slidesSelector = '.slide-list .slide-item';
-  const slidesControlsSelector = '.toggle-list .toggle-item';
-
-  let slides = document.querySelectorAll(slidesSelector);
-  let slidesControls = document.querySelectorAll(slidesControlsSelector);
-  let currentIndex = 0;
-  let slideInterval = setInterval(nextSlide, interval);
-
-  slidesControls.forEach(function(item, index) {
-    item.addEventListener('click', setCurrentSlide.bind(null, index))
-  });
-
-  function nextSlide() {
-    slides[currentIndex].classList.remove('current');
-    slidesControls[currentIndex].classList.remove('current');
-
-    currentIndex = (currentIndex+1)%slides.length;
-
-    slides[currentIndex].classList.add('current');
-    slidesControls[currentIndex].classList.add('current');
-  }
-
-  function setCurrentSlide(index) {
-    clearInterval(slideInterval);
-    document.querySelectorAll(`${slidesSelector}.current`)[0]
-      .classList.remove('current');
-    document.querySelectorAll(`${slidesControlsSelector}.current`)[0]
-      .classList.remove('current');
-
-    currentIndex = index;
-
-    slides[currentIndex].classList.add('current');
-    slidesControls[currentIndex].classList.add('current');
-    slideInterval = setInterval(nextSlide, interval);
-  }
+  const headerSlider = document.querySelectorAll('.header-slider')[0];
+  startSlider(headerSlider);
+  const quotesSlider = document.querySelectorAll('.quotes-slider')[0];
+  startSlider(quotesSlider);
 };
+
+function startSlider(slider) {
+    const interval = 3000;
+    const slidesSelector = '.slide-list .slide-item';
+    const slidesControlsSelector = '.toggle-list .toggle-item';
+
+    let slides = slider.querySelectorAll(slidesSelector);
+    let sliderControls = slider.querySelectorAll(slidesControlsSelector);
+
+    let currentIndex = 0;
+    let slideInterval = setInterval(nextSlide, interval);
+
+    sliderControls.forEach(function(item, index) {
+      item.addEventListener('click', setCurrentSlide.bind(null, index))
+    });
+
+    function nextSlide() {
+      slides[currentIndex].classList.remove('current');
+      sliderControls[currentIndex].classList.remove('current');
+
+      currentIndex = (currentIndex+1)%slides.length;
+
+      slides[currentIndex].classList.add('current');
+      sliderControls[currentIndex].classList.add('current');
+    }
+
+    function setCurrentSlide(index) {
+      clearInterval(slideInterval);
+      slider.querySelectorAll(`${slidesSelector}.current`)[0]
+        .classList.remove('current');
+      slider.querySelectorAll(`${slidesControlsSelector}.current`)[0]
+        .classList.remove('current');
+
+      currentIndex = index;
+
+      slides[currentIndex].classList.add('current');
+      sliderControls[currentIndex].classList.add('current');
+      slideInterval = setInterval(nextSlide, interval);
+    }
+}
